c3411f6801fef276b11a9b9ed54b7f8fd03232fd
祺噜噜 <jimmyshealisa@outlook.com>
2023-02-19 00:17:05 +0800

    修改导入包的问题

--- xdclass-order-service/src/main/java/net/xdclass/controller/CallBackController.java
+++ xdclass-order-service/src/main/java/net/xdclass/controller/CallBackController.java
@@ -2,10 +2,12 @@ package net.xdclass.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
